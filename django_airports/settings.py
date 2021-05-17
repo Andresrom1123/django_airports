5faa1b6fd2a3c6ab05d8674828bf8639fbbeb1ee
Andresrom1123 <andres2016_romero@hotmail.com>
2021-05-17 09:23:54 -0500

    added django-heroku

--- django_airports/settings.py
+++ django_airports/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
+
+django_heroku.settings(locals())
